04b52679a9a349111a5fb2f93dbed849d34f2e52
Chris Anderson <jchris@gmail.com>
2023-04-17 12:46:31 +0200

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,9 @@ function App() {
 
   async function rehydrateDiscovery(){
     await discovery.rehydrate()
+    console.log('rehydrated', discovery)
+    if (discovery.doc?.product) setProduct(discovery.doc.product)
+    if (discovery.doc?.customer) setCustomer(discovery.doc.customer)
     setCount(count + 1)
   }
 
@@ -91,7 +94,7 @@ function App() {
           </form>
         </div>
         <div className="w-full flex flex-wrap">
-          {discovery.personas.map((p) => (
+          {discovery.personas.filter((p) => p.description).map((p) => (
             <PersonaInterview persona={p}></PersonaInterview>
           ))}
         </div>
@@ -141,10 +144,10 @@ function FollowUp({ discovery }: any) {
           </button>
         </div>
       </form>
-      {discovery.interviewSummary && (
-        <p className="text=-black">{discovery.interviewSummary}</p>
+      {discovery.doc.interviewSummary && (
+        <p className="text=-black">{discovery.doc.interviewSummary}</p>
       )}
-      {discovery.followUps && <AskFollowUps discovery={discovery} />}
+      {discovery.doc?.followUps && <AskFollowUps discovery={discovery} />}
     </div>
   );
 }
@@ -160,7 +163,7 @@ function AskFollowUps({ discovery }) {
   return (
     <>
       <h3>Follow up questions</h3>
-      <p className="text=-black">{discovery.followUps}</p>
+      <p className="text=-black">{discovery.doc?.followUps}</p>
       <button onClick={doFollowUp}>Ask them</button>
     </>
   );
@@ -170,6 +173,16 @@ function PersonaInterview({ persona }: any) {
   const [perspective, setPerspective] = useState("");
   const [count, setCount] = useState(0);
 
+//   const product = await ux.prompt('What is the quick elevator pitch for your product.')
+//   const customer = await ux.prompt('Please describe your most clearly understood customer or initial user.')
+//   const channel = await ux.prompt('What is the primary channel through which you will reach your customer?')
+//   const pricing = await ux.prompt('What is the pricing model for your product?')
+//   const problem = await ux.prompt('What is the problem that your customer is trying to solve?')
+//   const dayInLife = await ux.prompt('What is a typical day in the life of your customer?')
+//   const buyerMapPeople = await ux.prompt('Who are the people that influence the buying decision?')
+//   const roiJustification = await ux.prompt('What is the ROI justification for your product?')
+//   const buyingHabits = await ux.prompt('What are the buying habits of your customer? What channels to the puchase through?')
+
   async function doInterview(e) {
     e.preventDefault();
     // console.log("do inteview", persona);
@@ -177,18 +190,22 @@ function PersonaInterview({ persona }: any) {
     await persona.conductInterview(
       "What are the biggest problems a product like this could help you solve?"
     );
+    setCount(count + 1);
     await persona.conductInterview("How much does this problem cost you?");
+    setCount(count + 1);
     await persona.conductInterview("What other solutions are you considering?");
+    setCount(count + 1);
     await persona.conductInterview(
       "If you could wave a magic wand and change anything about this, what would you do?"
     );
     // console.log(persona.conversation);
+    await persona.persist()
     setCount(count + 1);
   }
 
   return (
     <div className="w-full md:w-1/3 mb-8 md:pr-4" key={persona.description}>
-      <h2 className="text-lg font-bold text-gray-800 mb-4">Column 1</h2>
+      <h2 className="text-lg font-bold text-gray-800 mb-4">{persona.displayName()}</h2>
       <div className="bg-white rounded-lg shadow-lg p-4">
         <p className="text-gray-700 mb-2">{persona.description}</p>
 
@@ -228,6 +245,7 @@ function PersonaInterview({ persona }: any) {
 
 function PersonaConversation({ persona, conversation }: any) {
   // console.log(persona, conversation);
+  if (conversation.length === 0) return (<></>);
   const [first, ...rest] = conversation;
   return (
     <div
--- src/gpt.js
+++ src/gpt.js
@@ -1,38 +1,50 @@
-// import { ChatOpenAI } from "langchain/chat_models/openai";
-// import { HumanChatMessage, SystemChatMessage } from "langchain/schema";
+// global.process.env.OPENAI_API_KEY = import.meta.env.OPENAI_API_KEY
 
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { HumanChatMessage, SystemChatMessage } from "langchain/schema";
+// import {config} from 'dotenv'
+// config()
 import { Fireproof, Index } from "@fireproof/core";
 // import { useFireproof, FireproofCtx } from '@fireproof/core/hooks/use-fireproof'
 
-class HumanChatMessage {}
-class SystemChatMessage {}
+// class HumanChatMessage {}
+// class SystemChatMessage {}
 
-class ChatOpenAI {
-  call() {
-    return {
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, \nsed do eiusmod tempor incididunt ut \nlabore et dolore magna aliqua",
-    };
-  }
-}
+// class ChatOpenAI {
+//   call() {
+//     return {
+//       text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, \nsed do eiusmod tempor incididunt ut \nlabore et dolore magna aliqua",
+//     };
+//   }
+// }
 
 export class Discovery {
   constructor() {
     this.personas = [];
-    this.interviewer = new ChatOpenAI({ temperature: 0 });
+    this.interviewer = new ChatOpenAI({
+      temperature: 0,
+      openAIApiKey: "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",
+    });
     this.db = Fireproof.storage("epiphany");
-    window.db = this.db
-    this.typeIndex = new Index(this.db, (doc, map) => map(doc.type, null));
+    window.db = this.db;
+    this.typeIndex = new Index(this.db, (doc, map) => map(doc.type, doc));
+    this.doc = { _id: "discovery" };
   }
 
   async rehydrate() {
     // load personas from fireproof
     this.personas = await this.loadPersonas();
+    this.doc = await this.db
+      .get("discovery")
+      .catch((e) => ({ _id: "discovery" }));
   }
 
   async loadPersonas() {
     const res = await this.typeIndex.query({ key: "persona" });
     console.log("personas", res.rows);
-    return [];
+    return res.rows.map((r) =>
+      Persona.fromDoc(r.value, this.interviewer, this.db)
+    );
   }
 
   async askFollowUps(followups) {
@@ -42,6 +54,10 @@ export class Discovery {
   }
 
   async generateCustomers(product, customer) {
+    this.doc.product = product;
+    this.doc.customer = customer;
+    await this.db.put(this.doc);
+
     const gptPeople = await this.interviewer.call([
       new SystemChatMessage(
         `You have read The Four Steps to the Epiphany by Steven Gary Blank and you are ready to
@@ -54,13 +70,20 @@ export class Discovery {
           Product pitch: ${product} Customer description: ${customer}`
       ),
     ]);
-    this.personas = await Promise.all(
-      gptPeople.text.split("\n").map(async (p) => {
-        const persona = new Persona(p, this.interviewer, product, customer);
-        await persona.persist(this.db);
-        return persona;
-      })
-    );
+
+    const people = gptPeople.text.split("\n");
+
+    for (const person of people) {
+      const persona = new Persona(
+        person,
+        this.interviewer,
+        product,
+        customer,
+        this.db
+      );
+      await persona.persist();
+      this.personas.push(persona);
+    }
   }
 
   async generateInterviewSummary() {
@@ -75,13 +98,14 @@ export class Discovery {
         "Summarize your conversations, highlighting the customers, use-cases, and features that have the most commercial viability."
       ),
     ]);
-    this.interviewSummary = summary.text;
     const followUps = await this.interviewer.call([
       new HumanChatMessage(
         "What are the top 3 questions we should ask the next customer we interview?"
       ),
     ]);
-    this.followUps = followUps.text;
+    this.doc.interviewSummary = summary.text;
+    this.doc.followUps = followUps.text;
+    await this.db.put(this.doc);
   }
 }
 
@@ -90,26 +114,40 @@ export class Persona {
     "description product customer conversations perspective followUpsAnswer";
 
   perspective = "";
-  constructor(description, interviewer, product, customer) {
+  constructor(description, interviewer, product, customer, db) {
     this.interviewer = interviewer;
-    this.chat = new ChatOpenAI({ temperature: 0 });
+    this.chat = new ChatOpenAI({
+      temperature: 0,
+      openAIApiKey: "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",
+    });
+
     this.description = description;
     this.didAsk = false;
     this.conversations = [];
     this.product = product;
     this.customer = customer;
     this.id = null;
+    this.db = db;
   }
 
-  static fromDoc(doc, interviewer) {
-    const persona = new Persona(null, interviewer);
+  displayName() {
+    return this.description?.split("-")[0];
+  }
+
+  displayAbout() {
+    return this.description?.split("-")[1];
+  }
+
+  static fromDoc(doc, interviewer, db) {
+    const persona = new Persona(null, interviewer, null, null, db);
     Persona.docFields
       .split(" ")
       .forEach((field) => (persona[field] = doc[field]));
-    this.id = doc._id;
+    persona.id = doc._id;
+    return persona;
   }
 
-  async persist(db) {
+  async persist() {
     const doc = { type: "persona" };
     if (this.id) {
       doc._id = this.id;
@@ -119,13 +157,17 @@ export class Persona {
         doc[field] = this[field];
       }
     });
-    const resp = await db.put(doc);
+    // console.log('persist', doc)
+    const resp = await this.db.put(doc);
+    // console.log('resp', resp)
+
     this.id = resp.id;
   }
 
   async askFollowUps(followups) {
     const qAnswer = await this.chat.call(new HumanChatMessage(followups));
     this.followUpsAnswer = qAnswer.text;
+    await this.persist(this.db);
   }
 
   async conductInterview(question, thisConvo = null, rounds = 3) {
@@ -158,7 +200,7 @@ export class Persona {
       ),
       new HumanChatMessage(qAnswer.text),
     ]);
-
+    await this.persist()
     return this.conductInterview(furtherQuestions.text, thisConvo, rounds - 1);
   }
 }
